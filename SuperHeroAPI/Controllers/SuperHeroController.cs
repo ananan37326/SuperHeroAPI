e52a8039b45e3f60ee984e9b688e6ca5b1f56882
Tousif Tanjim Anan <anantousif@gmail.com>
2024-04-23 02:04:28 +0600

    Add project files.

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI.Services.SuperHeroService;
+
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly ISuperHeroService _superHeroService;
+
+        public SuperHeroController(ISuperHeroService superHeroService)
+        {
+            this._superHeroService = superHeroService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            var result = await this._superHeroService.GetAllHeroes();
+
+            if (result == null)
+            {
+                return NotFound("Heroes not found");
+            }
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetSingleHero(int id)
+        {
+            var result = await this._superHeroService.GetSingleHero(id);
+
+            if (result == null)
+            {
+                return NotFound("Hero not found");
+            }
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            var result = await this._superHeroService.AddHero(hero);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(int id, SuperHero request)
+        {
+            var result = await this._superHeroService.UpdateHero(id, request);
+
+            if (result is null)
+            {
+                return NotFound("Hero not found");
+            }
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id)
+        {
+            var result = await this._superHeroService.DeleteHero(id); 
+
+            if (result is null)
+            {
+                return NotFound("Hero not found");
+            }
+            return Ok(result);
+        }
+    }
+}
